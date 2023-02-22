c1bca659106c1fc46c781bcb4e79c675b24215ad
Shakuntala Priya <spriya@nownow.ng>
2023-02-22 15:09:07 +0530

    Added a sout in Sum.java

--- Sum.java
+++ Sum.java
@@ -1,8 +1,8 @@
-public class Sum
-{
+public class Sum{
     public static void main(String[] args) {
         int a=5;
         int b=8;
         System.out.println("The sum is:"+ (a+b));
+        System.out.println("i want to make some changes and commit again!");
     }
 }
